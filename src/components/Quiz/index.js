aa818454f85e1dd73e17e7dd6fe9175a72ed202a
Victor Lombard <victor@lombard.fr>
2024-11-06 19:05:54 +0100

    WIP: Add Quiz, Levels, and Progressbar components

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,14 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Levels from '../Levels';
+import ProgressBar from '../ProgressBar';
+import { QuizMarvel } from '../quizMarvel';
 
 class Quiz extends Component {
 
+  state = {
+    levelNames: ["debutant", "confirme", "expert"],
+    quizLevel: 0,
+    maxQuestions: 10,
+    storedQuestions: [],
+    question: null,
+    options: [],
+    idQuestion: 0
+  }
+
+  loadQuestions = (level) => {
+    const fetchedArray = QuizMarvel[0].quizz[level]
+    console.log(fetchedArray);
+    if (fetchedArray.length >= this.state.maxQuestions) {
+      const newArray = fetchedArray.map(({answer, ...keepRest}) => keepRest)
+
+      this.setState({
+        storedQuestions: newArray
+      })
+    } else {
+      console.log("Pas assez de questions !!!")
+    }
+  }
+
+  componentDidMount() {
+    this.loadQuestions(this.state.levelNames[this.state.quizLevel]);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.storedQuestions !== prevState.storedQuestions) {
+        this.setState({
+          question: this.state.storedQuestions[this.state.idQuestion].question,
+          options: this.state.storedQuestions[this.state.idQuestion].options
+        })
+    }
+  }
+
   render() {
 
-    const {pseudo} = this.props.userData
+    const displayOptions = this.state.options.map((option, index) => {
+      return (
+        <p key={index} className='answerOptions'>{option}</p>
+
+      )
+    })
+
+    // const {pseudo} = this.props.userData
 
     return (
       <div>
-        <h2>Pseudo: {pseudo}</h2>
+        <Levels />
+        <ProgressBar />
+        <h2>{this.state.question}</h2>
+        {displayOptions}
+        <button className='btnSubmit'>Suivant</button>
       </div>
     )
   }
