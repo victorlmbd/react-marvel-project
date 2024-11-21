f3e8a3becd84453c584e49820f9a6a7b51949931
Victor Lombard <victor@lombard.fr>
2024-11-21 17:47:14 +0100

    Set up environment and clean up HTML and files

--- src/components/Firebase/firebaseConfig.js
+++ src/components/Firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getFirestore, doc } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiZU7u7ghXI9w1_sDGWo1lzmjgFGCWAtE",
-  authDomain: "marvel-quiz-263d2.firebaseapp.com",
-  projectId: "marvel-quiz-263d2",
-  storageBucket: "marvel-quiz-263d2.appspot.com",
-  messagingSenderId: "873972161871",
-  appId: "1:873972161871:web:b3771d0c09781f2881393a"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
